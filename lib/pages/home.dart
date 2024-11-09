1aa176d0ebf54af04e3206c386ff995010c57f1d
jessjoseph <jessica.004j@gmail.com>
2024-11-09 14:08:59 +0530

    updated home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,12 +13,35 @@ class _HomeState extends State<Home>{
     return Scaffold(
       appBar: AppBar(
         title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [Text("News"),Text("Nest", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold )
           ),
           ],
         ),
+        centerTitle: true,
+        elevation: 0.0,
       ),
       body: Container(),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final image, categoryName;
+  CategoryTile({this.categoryName, this.image});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(children:[
+        Image.asset(
+            image,
+            width:120,
+            height: 60,
+        )
+      ],
+      ),
     );
   }
 }
