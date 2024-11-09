d89621bce08ffe6e9460beb3f40752a25a3eb8b8
jessjoseph <jessica.004j@gmail.com>
2024-11-09 19:06:13 +0530

    slider commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import "package:flutter/material.dart";
+import 'package:untitled/models/slider_model.dart';
+import 'package:untitled/services/slider_data.dart';
 import '../models/category_model.dart';
 import '../services/data.dart';
 
@@ -11,11 +14,14 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home>{
   List<CategoryModel> categories=[];
+  List<sliderModel> sliders=[];
   @override
   void initState(){
-    categories=getCategories();
+    categories= getCategories();
+    sliders=getSliders();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,8 +39,11 @@ class _HomeState extends State<Home>{
         child: Column(
             children: [
               Container(
+                margin: EdgeInsets.only(left: 10.0),
+                height: 70,
                 child: ListView.builder(
                   shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
                     itemCount: categories.length,
                     itemBuilder: (context, index){
                       return CategoryTile(
@@ -42,12 +51,23 @@ class _HomeState extends State<Home>{
                         categoryName: categories[index].categoryName,
                       );
                     }),
-              )
-        ])
+              ),
+              CarouselSlider.builder(itemCount: sliders.length , itemBuilder: (context,index,realIndex){
+                String? res= sliders[index].image;
+                String? res1=sliders[index].name;
+                return buildImage(res!, index, res1!);
+              }, options: CarouselOptions(height:200,autoPlay: true, enlargeStrategy:CenterPageEnlargeStrategy.height )),
+        ],
+        ),
       ),
     );
   }
+  Widget buildImage(String urlImage, int index, String name)=>Container(
+    child: Image.asset(urlImage, fit: BoxFit.cover, width:MediaQuery.of(context).size.width),
+  );
 }
+
+
 
 class CategoryTile extends StatelessWidget {
   final image, categoryName;
@@ -56,13 +76,31 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children:[
-      Image.asset(
+    return Container(
+      margin: EdgeInsets.only(right: 16),
+      child: Stack(
+      children:[
+        ClipRRect(
+          borderRadius: BorderRadius.circular(6),
+
+        child: Image.asset(
           image,
           width:120,
-          height: 60,
+          height: 70,
+          fit: BoxFit.cover,
+        )
+        ),
+        Container(
+            width: 120,
+            height: 70,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(6),color: Colors.black26,),
+
+          child: Center(child:Text(categoryName, style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold)
+          ))
+        )
+      ],
       )
-    ],
     );
   }
+ 
 }
---
+++ lib/services/slider_data.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:untitled/models/slider_model.dart';
+
+List<sliderModel> getSliders(){
+
+  List<sliderModel>slider=[];
+  sliderModel categoryModel= new sliderModel();
+
+  categoryModel.image="images/business.jpg";
+  categoryModel.name="Bow To The Authority of Silenforce";
+  slider.add(categoryModel);
+  categoryModel= new sliderModel();
+
+  categoryModel.image="images/entertainment.jpg";
+  categoryModel.name="Bow To The Authority of Silenforce";
+  slider.add(categoryModel);
+  categoryModel= new sliderModel();
+
+  categoryModel.image="images/health.jpg";
+  categoryModel.name="Bow To The Authority of Silenforce";
+  slider.add(categoryModel);
+  categoryModel= new sliderModel();
+
+  return slider;
+}
