255e19655cd733b227886d16502734693003aae1
jessjoseph <jessica.004j@gmail.com>
2024-11-16 12:23:40 +0530

    home page updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:news_snack/view/home.dart';
-import 'package:news_snack/view/spalsh.dart';
+import 'package:newsnest/lib/view/home.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -34,7 +34,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'News Snack',
+      title: 'NewsNest',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
